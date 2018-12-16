b01ef11b0bd9e611c4a936e54866464181e243a4
satish <satishkumar432@gmail.com>
2018-12-16 03:08:19 +0000

    yml changes

--- initial-setup.sh
+++ initial-setup.sh
@@ -5,4 +5,8 @@ yum install -y vim
 systemctl enable docker
 systemctl start docker
 systemctl status docker
+ echo "autocmd FileType yaml setlocal ai ts=2 sw=2 et" > ~/.vimrc
+ echo "set number" >> ~/.vimrc
+ echo "set cursorcolumn" >> ~/.vimrc
+
 
